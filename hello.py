3660b89bd04ed852f59d883166876bec24d21e5e
balagesh <balagesh.h@gmail.com>
2025-01-10 09:16:08 +0000

    os methods in hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,9 @@ import os
 
 username = os.getenv("USER")
 print(f"Hello, {username}!")
+print("current working directory: "+str((os.getcwd())))
+print("current process' real user id: "+str(os.getuid()))
+print("directories in which the system looks for named exec progs: ")
+for x in (os.get_exec_path()):
+    print("* "+x)
+print(("size of terminal: "+str(os.get_terminal_size())))
